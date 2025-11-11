c61599591695f44afb8c53819cebc8c252001bff
mohamedhassan <mohamedhassanamara7@gmail.com>
2025-11-11 23:12:41 +0100

    added insights

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Layout from '../components/Layout';
 import { LangProvider } from '../contexts/LangContext';
 import '../styles/globals.css';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
     return (
@@ -32,6 +33,7 @@ function MyApp({ Component, pageProps }) {
                 <Component {...pageProps} />
             </Layout>
             <Analytics />
+            <SpeedInsights />
         </LangProvider>
     );
 }
